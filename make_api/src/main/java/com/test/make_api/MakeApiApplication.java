81f6618da276d1d2d7a961066b9195c3273525fa
day0ung <jdy9436@naver.com>
2023-06-30 15:23:01 +0900

    Initial commit

---
+++ make_api/src/main/java/com/test/make_api/MakeApiApplication.java
@@ -0,0 +1,13 @@
+package com.test.make_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeApiApplication.class, args);
+	}
+
+}
