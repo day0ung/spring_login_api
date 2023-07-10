91d7f653eff9bb05131d0606a559a85d0ad20897
day0ung <jdy9436@naver.com>
2023-07-10 18:37:13 +0900

    feat :  oauth google 후처리 기능

--- api/src/main/java/com/made/api/config/security/SecurityConfiguration.java
+++ api/src/main/java/com/made/api/config/security/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.made.api.config.security;
 
 
+import com.made.api.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityBuilder;
@@ -16,6 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // @Secured,  (@PreAuthorize, @PostAuthorize)어노테이션 활성화
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     @Bean // 해당 메서드의 리턴되는 오브젝트를 Ioc로 등록
     public BCryptPasswordEncoder encodePassword(){
@@ -38,8 +43,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/login") // Login주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행 controller에서 /login을 만들지않아도됨
                 .defaultSuccessUrl("/")
                 .and()
+                /*
+                    1. 코드받기(인증)
+                    2. 엑세스토큰 받기(사용자정보에 접근할 권한)
+                    3. 권할을 통해 사용자 프로필정보를 가져옴
+                    4. 정보를 토대로 회원가입 자동진행
+                    4-1. (이메일, 전화번호,이름,아이디) 추가 정보가 필요한 경우 추가 회원가입 프로세스
+                 */
                 .oauth2Login()
                 .loginPage("/loginForm") //구글 로그인이 완료된 후 후처리 필요
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService) // Tip. 구글 로그인이 완료가 되면 (AccessToken + 사용자 프로필 정보)
                 ;
 
     }
