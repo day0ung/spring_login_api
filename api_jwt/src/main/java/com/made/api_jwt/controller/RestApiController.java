6558866c82441cc0fe503cb351a9bb58c0223111
day0ung <jdy9436@naver.com>
2023-07-16 17:13:49 +0900

    feat : join add

--- api_jwt/src/main/java/com/made/api_jwt/controller/RestApiController.java
+++ api_jwt/src/main/java/com/made/api_jwt/controller/RestApiController.java
@@ -1,25 +1,43 @@
 package com.made.api_jwt.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.made.api_jwt.model.User;
+import com.made.api_jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 public class RestApiController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     @GetMapping("home")
     public String home(){
         return "<h1>HOME</h1>";
     }
 
-    /*
-    "error": "Method Not Allowed",
-    "path": "/home"
-     */
     @PostMapping("token")
     public String token(){
         return "<h1>Token</h1>";
     }
+
+    @PostMapping("join")
+    public String join(@RequestBody User user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("USER");
+        userRepository.save(user);
+        return "<h1>Join</h1>";
+
+        /*
+        Headers Content-Type = application/json
+         */
+
+    }
+
+
 
 
 }
